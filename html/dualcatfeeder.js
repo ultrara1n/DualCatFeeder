f9bd08f0dbb4315238f33b27e8f7c21dee738818
Philipp Wensauer <mail@philippwensauer.com>
2020-05-29 01:11:33 +0200

    websocket client example

---
+++ html/dualcatfeeder.js
@@ -0,0 +1,18 @@
+
+var connection = new WebSocket('ws://192.168.178.76:81/');
+connection.onopen = function () {
+  connection.send('Connect ' + new Date());
+};
+connection.onerror = function (error) {
+  console.log('WebSocket Error ', error);
+};
+connection.onmessage = function (e) {
+  console.log('Server: ', e.data);
+};
+connection.onclose = function () {
+  console.log('WebSocket connection closed');
+};
+
+function sendMessage () {
+  connection.send("MS1");
+}
